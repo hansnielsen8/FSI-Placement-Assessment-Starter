6f091054e4702246d63d696e3448e26be72ce316
hans nielsen <hk32077@gmail.com>
2022-02-08 19:26:13 -0600

    Finished

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Hans Nielsen" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
@@ -25,3 +25,47 @@ console.log('Gingerbread + button was clicked!')
 })
 
 // TODO: Hook up event listeners for the rest of the buttons
+
+
+
+//Chocolate Chip Cookie
+
+let ccPlusBtn = document.querySelector('#add-cc')
+let ccQuant = document.querySelector('#qty-cc')
+let totalQuant = document.querySelector('#qty-total')
+ccPlusBtn.addEventListener('click', function() {
+
+    cc++
+    console.log("cc button click")
+    ccQuant.textContent = `${cc}`
+    totalQuant.textContent = `${cc}`
+})
+
+//Subtract CC
+
+let ccMinusBtn = document.querySelector('#minus-cc')
+
+ccMinusBtn.addEventListener('click', function() { 
+    cc--
+    ccQuant.textContent = `${cc}`
+    console.log('minus button click')
+})
+
+
+// add Sugar Cookie
+
+let sugarPlus = document.querySelector('#add-sugar')
+let sugarQuant = document.querySelector('#qty-sugar')
+
+sugarPlus.addEventListener('click', function() {
+    sugar++
+    sugarQuant.textContent = `${sugar}`
+})
+
+// subtract Sugar Cookie
+let sugarMinus = document.querySelector('#minus-sugar')
+
+sugarMinus.addEventListener('click', function() {
+    sugar--
+    sugarQuant.textContent = `${sugar}`
+})
